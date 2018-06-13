8a7e02733b1fbda8bea15eaa3900ac0ad368e22d
Vinisharma11 <sharvi786@gmail.com>
2018-06-13 15:36:54 +0530

    second commit

--- overloading.java
+++ overloading.java
@@ -1,30 +1,22 @@
-class age
-{
-	public static void info(int age)
-	{
-     System.out.println("age:"+age);
-	}
-}
-class score
-{
-     public static void info(float percent)
-     {
-		System.out.println("percentage:"+percent);
-	 }
-}
+
 class information
-{
-	 public static void info()
-	 {
-	 	System.out.println("student detail");
-	 	System.out.println("name: Vinita");
-	 }
+{  
+	public static void print(int i)
+	{
+		System.out.println(i);
+    }
+   public static void print(float r)
+    {
+    	System.out.println(r);
+    }
+    public static void print()
+    {
+    	System.out.println(" method overloading");
+    }
 	 public static void main(String [] args)
 	 {
-	 	age a= new age();
-	 	score s= new score();
-	 	info();
-	 	a.info(20);
-	 	s.info(89.6);
+	 	print(10);
+	 	print(14.5f);
+	 	print();
 	 }
 }
