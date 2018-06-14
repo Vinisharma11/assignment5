6030b749d609ec261b6f34c01261c8f11ea8d2eb
Vinisharma11 <sharvi786@gmail.com>
2018-06-14 16:44:04 +0530

    converting string to integer

---
+++ convert.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class convert
+{
+	public static void main(String []args)
+	{
+		int a= Integer.valueOf("200");
+		int b = Integer.valueOf("600");
+	if(a<b)
+	{
+		System.out.println("MAXIMUM OF TWO IS "+b);
+	}
+	else
+	{
+		System.out.println("Maximum of two is "+a);
+	}
+	}
+	
+	
+}
